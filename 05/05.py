46a7b4450cb84341dd571f818a03aac16b8458dc
Job Ammerlaan <job.ammerlaan@transavia.com>
2019-12-05 21:16:21 +0100

    Finished part 2; extremely difficult challenge for me.
    This challenge gave me a very hard time until I found out it was simple off-by-one errors that were tripping me up. Lots of printing and debugging until I figured out what my problems were; mostly setting the wrong output value (i+4 instead of i+3) and misnumbering the number of params for some operators. Did some major refactoring of working, but ugly code after I got the correct answer. May have to revisit this some day!

--- 05/05.py
+++ 05/05.py
@@ -2,52 +2,46 @@ from helper_functions.io import read_input_file
 
 DAY = '05'
 input_string = read_input_file(DAY)
-# input_string = "1002,4,3,4,33"
 base_intcode = list(map(int, input_string.split(',')))
 
 
-def get_arg(intcode, i, mode):
-    print('mode {}, index {}, value {}'.format(mode, i, intcode[i] if mode == 0 else i))
-    if mode == 0:
-        return intcode[i]
-    else:
-        return i
-
-
 def get_op_modes(op_str):
-    op_str = op_str.zfill(4)  # pad opcode to length 5
-    modes = list(map(int, op_str[0:2]))  # get modes as integers
-    return int(op_str[2:]), modes[::-1]  # reverse the modes list
+    op_str = op_str.zfill(5)  # pad opcode to length 5 so we can always use it the same way
+    modes = list(map(int, op_str[0:3]))  # get modes as ints
+    return int(op_str[3:]), modes[::-1]  # reverse the modes list
 
 
-def run_program2(base_intcode, input_val):
-    intcode = base_intcode.copy()
+def run_program(intcode, input_val):
     i = 0
-    while True:
-        # print('Iterating... i={}'.format(i))
-        # print("Intcode: {}".format(intcode[i:]))
-        # print("Relevant bit: {}".format(intcode[i:i + 4]))
+    num_params = [0, 3, 3, 1, 1, 2, 2, 3, 3]
+
+    while intcode[i] != 99:
         op, modes = get_op_modes(str(intcode[i]))
-        # print(op, modes)
-        if op <= 2:
-            args = intcode[i + 1:i + 3]
-            for j, mode in enumerate(modes):
-                args[j] = intcode[args[j]] if mode == 0 else args[j]
-            # print("Args: {}".format(args))
-            if op == 1:
-                intcode[i + 3] = args[0] + args[1]
-            if op == 2:
-                intcode[i + 3] = args[0] * args[1]
-            i += 4
+        # Refactored my get_args() function after seeing the list comprehension from /u/lele3000:
+        args = [intcode[i + j + 1] if modes[j] else intcode[intcode[i + j + 1]] for j in range(num_params[op])]
+        if op == 1:
+            intcode[intcode[i + 3]] = args[0] + args[1]
+        elif op == 2:
+            intcode[intcode[i + 3]] = args[0] * args[1]
         elif op == 3:
             intcode[intcode[i + 1]] = input_val
-            i += 2
         elif op == 4:
-            print(intcode[i + 1])
-            i += 2
+            intcode[0] = intcode[intcode[i + 1]]  # use intcode[0] as output value since it's never (re)used
+        elif op == 5:
+            i = args[1] - 3 if args[0] != 0 else i  # offset the increment at the end of this loop by subtracting 3
+        elif op == 6:
+            i = args[1] - 3 if args[0] == 0 else i  # offset the increment at the end of this loop by subtracting 3
+        elif op == 7:
+            intcode[intcode[i + 3]] = 1 if args[0] < args[1] else 0
+        elif op == 8:
+            intcode[intcode[i + 3]] = 1 if args[0] == args[1] else 0
         else:
-            assert op == 99
-            break
+            raise ValueError('Invalid operator code {}'.format(op))
+        i += num_params[op] + 1
+    return intcode[0]
 
 
-run_program2(base_intcode=base_intcode, input_val=1)
+# part one
+print(run_program(intcode=base_intcode.copy(), input_val=1))
+# part two
+print(run_program(intcode=base_intcode.copy(), input_val=5))
