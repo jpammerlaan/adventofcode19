2cd62fb5d754980e13b85b4318dea0e95d2f5ed6
Job Ammerlaan <job.ammerlaan@transavia.com>
2019-12-06 16:02:47 +0100

    Refactored some more, saving calls to get_orbits()

--- 06/06.py
+++ 06/06.py
@@ -1,23 +1,20 @@
 from helper_functions.io import read_input_file
-from collections import defaultdict
 
 DAY = '06'
 orbit_map = read_input_file(DAY, output_type='list')
 
 
-def get_orbits(orbits, obj):
-    orb = [obj]
-    while orbits[obj] != 'COM':
-        obj = orbits[obj]
-        orb.append(obj)
-    return orb
+def get_orbits(direct_orbits, obj):
+    orbits = [obj]
+    while direct_orbits[obj] != 'COM':
+        obj = direct_orbits[obj]
+        orbits.append(obj)
+    return orbits
 
 
-direct_orbits = {b: a for a, b in map(lambda o: o.split(')'), orbit_map)}
-indirect_orbits = [get_orbits(direct_orbits, obj) for obj in direct_orbits]
+orbits_dict = {b: a for a, b in map(lambda o: o.split(')'), orbit_map)}
+indirect_orbits = {obj: get_orbits(orbits_dict, obj) for obj in orbits_dict.keys()}
 # part one
-print(sum([len(o) for o in indirect_orbits]))
+print(sum([len(o) for o in indirect_orbits.values()]))
 # part two
-you = set(get_orbits(direct_orbits, direct_orbits['YOU']))
-san = set(get_orbits(direct_orbits, direct_orbits['SAN']))
-print(len(you.symmetric_difference(san)))
+print(len(set(indirect_orbits[orbits_dict['YOU']]).symmetric_difference(indirect_orbits[orbits_dict['SAN']])))
