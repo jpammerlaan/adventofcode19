a755dba503e995aca5017799926e5a5c4d3d73dd
Job Ammerlaan <job.ammerlaan@transavia.com>
2019-12-08 14:16:11 +0100

    temp commit

---
+++ 07/07.py
@@ -0,0 +1,62 @@
+from helper_functions.io import read_input_file
+from itertools import permutations
+
+DAY = '07'
+input_string = read_input_file(DAY)
+base_intcode = list(map(int, input_string.split(',')))
+
+
+def get_op_modes(op_str):
+    op_str = op_str.zfill(5)  # pad opcode to length 5 so we can always use it the same way
+    modes = list(map(int, op_str[0:3]))  # get modes as ints
+    return int(op_str[3:]), modes[::-1]  # reverse the modes list
+
+
+def run_program(intcode, phase, input_val):
+    i = 0
+    num_params = [0, 3, 3, 1, 1, 2, 2, 3, 3]
+    used_phase = False
+
+    while intcode[i] != 99:
+        op, modes = get_op_modes(str(intcode[i]))
+        # Refactored my get_args() function after seeing the list comprehension from /u/lele3000:
+        args = [intcode[i + j + 1] if modes[j] else intcode[intcode[i + j + 1]] for j in range(num_params[op])]
+        if op == 1:
+            intcode[intcode[i + 3]] = args[0] + args[1]
+        elif op == 2:
+            intcode[intcode[i + 3]] = args[0] * args[1]
+        elif op == 3:
+            intcode[intcode[i + 1]] = input_val if used_phase else phase
+            used_phase = True
+        elif op == 4:
+            intcode[0] = intcode[intcode[i + 1]]  # use intcode[0] as output value since it's never (re)used
+        elif op == 5:
+            i = args[1] - 3 if args[0] != 0 else i  # offset the increment at the end of this loop by subtracting 3
+        elif op == 6:
+            i = args[1] - 3 if args[0] == 0 else i  # offset the increment at the end of this loop by subtracting 3
+        elif op == 7:
+            intcode[intcode[i + 3]] = 1 if args[0] < args[1] else 0
+        elif op == 8:
+            intcode[intcode[i + 3]] = 1 if args[0] == args[1] else 0
+        else:
+            raise ValueError('Invalid operator code {}'.format(op))
+        i += num_params[op] + 1
+    return intcode[0]
+
+
+def find_highest(phase_range):
+    phase_perms = permutations(phase_range)
+    highest = 0
+    input_val = 0
+    for phases in phase_perms:
+        for phase in phases:
+            input_val = run_program(base_intcode.copy(), phase, input_val)
+            print(input_val)
+        highest = input_val if input_val > highest else highest
+    return highest
+
+
+# part one
+# print(find_highest(phase_range=range(0, 5)))
+# part two
+print(find_highest(phase_range=range(5, 10)))
