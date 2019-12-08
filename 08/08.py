3abe2b2a2bc547dc4a82b305f27b741b77e21206
Job Ammerlaan <job.ammerlaan@transavia.com>
2019-12-08 15:26:38 +0100

    Added day 8, easy challenge again.
    Not much to say, refactored some of the recursive code and created a
    chunker function at the end since it's used a second time in part two.
    I'm finding these non-Intcode parser challenges a lot harder than
    yesterday's trainwreck of a challenge though...

---
+++ 08/08.py
@@ -0,0 +1,25 @@
+from helper_functions.io import read_input_file
+
+DAY = '08'
+pixels_raw = read_input_file(DAY, output_type='string')
+H = 6
+W = 25
+
+
+def chunk(x, size):
+    return [x[i:i + size] for i in range(0, len(x), size)]
+
+
+def get_pixel(layers, i, depth=0):
+    return get_pixel(layers, i, depth+1) if layers[depth][i] == '2' else layers[depth][i]
+
+
+layers = chunk(pixels_raw, H*W)
+# part one
+check_layer = layers[min(map(lambda x: x.count('0'), layers))]
+print(check_layer.count('1') * check_layer.count('2'))
+# part two
+pixels_list = list(map(lambda i: get_pixel(layers, i), range(H*W)))
+pixels_grid = chunk(pixels_list, W)
+print('\n'.join(''.join(['#' if p == '1' else ' ' for p in row]) for row in pixels_grid))
+
