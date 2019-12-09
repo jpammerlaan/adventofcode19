6ee8028051013ce9436627444bb63fbe59a01de2
Job Ammerlaan <job.ammerlaan@transavia.com>
2019-12-09 17:19:53 +0100

    Temp commit for day 09

---
+++ 09/09.py
@@ -0,0 +1,73 @@
+from helper_functions.io import read_input_file
+
+DAY = '09'
+input_string = read_input_file(DAY)
+base_intcode = list(map(int, input_string.split(',')))
+
+
+class Program:
+    NUM_PARAMS = [0, 3, 3, 1, 1, 2, 2, 3, 3]
+
+    def __init__(self, intcode, phase, return_output):
+        self.intcode = intcode
+        self.phase = phase
+        self.used_phase = False
+        self.active = True
+        self.input = None
+        self.output = None
+        self.pause_on_output = return_output
+        self.idx = 0
+
+    @staticmethod
+    def _get_op_modes(op_str):
+        op_str = op_str.zfill(5)  # pad opcode to length 5 so we can always use it the same way
+        modes = list(map(int, op_str[0:3]))  # get modes as ints
+        return int(op_str[3:]), modes[::-1]  # reverse the modes list
+
+    def set_input(self, input):
+        self.input = input
+
+    def get_output(self):
+        return self.output
+
+    def get_active_status(self):
+        return self.active
+
+    def run(self):
+        while self.intcode[self.idx] != 99:
+            op, modes = self._get_op_modes(str(self.intcode[self.idx]))
+            # Refactored my get_args() function after seeing the list comprehension from /u/lele3000:
+            args = [self.intcode[self.idx + j + 1] if modes[j] else self.intcode[self.intcode[self.idx + j + 1]] for j
+                    in range(self.NUM_PARAMS[op])]
+            if op == 1:
+                self.intcode[self.intcode[self.idx + 3]] = args[0] + args[1]
+            elif op == 2:
+                self.intcode[self.intcode[self.idx + 3]] = args[0] * args[1]
+            elif op == 3:
+                self.intcode[self.intcode[self.idx + 1]] = self.input if self.used_phase else self.phase
+                self.used_phase = True
+            elif op == 4:
+                self.output = self.intcode[self.intcode[self.idx + 1]]
+                if self.pause_on_output:
+                    self.idx += self.NUM_PARAMS[op] + 1
+                    return
+            elif op == 5:
+                # offset the increment at the end of this loop by subtracting 3
+                self.idx = args[1] - 3 if args[0] != 0 else self.idx
+            elif op == 6:
+                # offset the increment at the end of this loop by subtracting 3
+                self.idx = args[1] - 3 if args[0] == 0 else self.idx
+            elif op == 7:
+                self.intcode[self.intcode[self.idx + 3]] = 1 if args[0] < args[1] else 0
+            elif op == 8:
+                self.intcode[self.intcode[self.idx + 3]] = 1 if args[0] == args[1] else 0
+            else:
+                raise ValueError('Invalid operator code {}'.format(op))
+            self.idx += self.NUM_PARAMS[op] + 1
+        self.active = False
+        return
+
+
+boost = Program(intcode=base_intcode.copy(), phase=None, return_output=False)
+boost.used_phase = True # override phase setting
+boost.set_input(1)
