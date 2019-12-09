f35a80974b21a0591e501a679823e99af07b8c50
Job Ammerlaan <job.ammerlaan@transavia.com>
2019-12-09 21:38:58 +0100

    Finished day 09.
    The thing with these Intcode compilers is that once I have it working, it all seems to make sense. But the debugging is hell... At least we have a working Intcode computer now, so I should no longer need to worry about that. Also, I refactored day 05 to use the current version of our Intcode computer (that I now call the Program class), so day 05 and day 09 are basically identical now. Could (read: should, but probably won't) refactor day 07 as well.

--- 05/05.py
+++ 05/05.py
@@ -1,47 +1,16 @@
 from helper_functions.io import read_input_file
+from intcode.program import Program
 
 DAY = '05'
 input_string = read_input_file(DAY)
 base_intcode = list(map(int, input_string.split(',')))
 
-
-def get_op_modes(op_str):
-    op_str = op_str.zfill(5)  # pad opcode to length 5 so we can always use it the same way
-    modes = list(map(int, op_str[0:3]))  # get modes as ints
-    return int(op_str[3:]), modes[::-1]  # reverse the modes list
-
-
-def run_program(intcode, input_val):
-    i = 0
-    num_params = [0, 3, 3, 1, 1, 2, 2, 3, 3]
-
-    while intcode[i] != 99:
-        op, modes = get_op_modes(str(intcode[i]))
-        # Refactored my get_args() function after seeing the list comprehension from /u/lele3000:
-        args = [intcode[i + j + 1] if modes[j] else intcode[intcode[i + j + 1]] for j in range(num_params[op])]
-        if op == 1:
-            intcode[intcode[i + 3]] = args[0] + args[1]
-        elif op == 2:
-            intcode[intcode[i + 3]] = args[0] * args[1]
-        elif op == 3:
-            intcode[intcode[i + 1]] = input_val
-        elif op == 4:
-            intcode[0] = intcode[intcode[i + 1]]  # use intcode[0] as output value since it's never (re)used
-        elif op == 5:
-            i = args[1] - 3 if args[0] != 0 else i  # offset the increment at the end of this loop by subtracting 3
-        elif op == 6:
-            i = args[1] - 3 if args[0] == 0 else i  # offset the increment at the end of this loop by subtracting 3
-        elif op == 7:
-            intcode[intcode[i + 3]] = 1 if args[0] < args[1] else 0
-        elif op == 8:
-            intcode[intcode[i + 3]] = 1 if args[0] == args[1] else 0
-        else:
-            raise ValueError('Invalid operator code {}'.format(op))
-        i += num_params[op] + 1
-    return intcode[0]
-
-
 # part one
-print(run_program(intcode=base_intcode.copy(), input_val=1))
+p = Program(program=base_intcode.copy())
+p.run(1)
+print(p.get_output())
+
 # part two
-print(run_program(intcode=base_intcode.copy(), input_val=5))
+p = Program(program=base_intcode.copy())
+p.run(5)
+print(p.get_output())
---
+++ intcode/program.py
@@ -0,0 +1,59 @@
+class Program:
+    NUM_PARAMS = [0, 3, 3, 1, 1, 2, 2, 3, 3, 1]
+
+    def __init__(self, program):
+        self.program = program + [0] * 10000  # pad the intcode with an arbitrarily long list
+        self.output = None
+        self.relative_base = 0
+        self.idx = 0
+
+    @staticmethod
+    def _get_op_modes(op_str):
+        op_str = op_str.zfill(5)  # pad opcode to length 5 so we can always use it the same way
+        modes = list(map(int, op_str[0:3]))  # get modes as ints
+        return int(op_str[3:]), modes[::-1]  # reverse the modes list
+
+    def get_output(self):
+        return self.output
+
+    def __get_param(self, p, mode):
+        if mode == 2:
+            return self.relative_base + self[p]
+        elif mode == 0:
+            return self[p]
+        else:
+            return p
+
+    def __getitem__(self, index):
+        return self.program[index]
+
+    def __setitem__(self, index, val):
+        self.program[index] = val
+
+    def run(self, input_val):
+        while self[self.idx] != 99:
+            op, modes = self._get_op_modes(str(self[self.idx]))
+            params = [self.__get_param(self.idx + j + 1, modes[j]) for j in range(self.NUM_PARAMS[op])]
+            if op == 1:
+                self[params[2]] = self[params[0]] + self[params[1]]
+            elif op == 2:
+                self[params[2]] = self[params[0]] * self[params[1]]
+            elif op == 3:
+                self[params[0]] = input_val
+            elif op == 4:
+                self.output = self[params[0]]
+            elif op == 5:
+                if self[params[0]] != 0:
+                    self.idx = self[params[1]] - 3
+            elif op == 6:
+                if self[params[0]] == 0:
+                    self.idx = self[params[1]] - 3
+            elif op == 7:
+                self[params[2]] = int(self[params[0]] < self[params[1]])
+            elif op == 8:
+                self[params[2]] = int(self[params[0]] == self[params[1]])
+            elif op == 9:
+                self.relative_base += self[params[0]]
+            else:
+                raise ValueError('Invalid operator code {}'.format(op))
+            self.idx += self.NUM_PARAMS[op] + 1
