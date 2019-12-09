547c46b0f42d0abb15dc0364379a0dae0b2f4d17
jpammerlaan <jpammerlaan@gmail.com>
2019-12-09 21:40:58 +0100

    Feature/day 09 (#8)
    * Finished day 09.
    
    The thing with these Intcode compiler challenges is that once I have it working, it all seems to make sense. But the debugging is hell... At least I have a working Intcode computer now, so I should no longer need to worry about that. Also, I refactored day 05 to use the current version of our Intcode computer (that I now call the Program class), so day 05 and day 09 are basically identical now. Could (read: should, but probably won't) refactor day 07 as well.

--- 09/09.py
+++ 09/09.py
@@ -1,16 +1,16 @@
 from helper_functions.io import read_input_file
 from intcode.program import Program
 
-DAY = '05'
+DAY = '09'
 input_string = read_input_file(DAY)
 base_intcode = list(map(int, input_string.split(',')))
 
 # part one
-p = Program(program=base_intcode.copy())
-p.run(1)
-print(p.get_output())
+boost = Program(program=base_intcode.copy())
+boost.run(1)
+print(boost.get_output())
 
 # part two
-p = Program(program=base_intcode.copy())
-p.run(5)
-print(p.get_output())
+boost_two = Program(program=base_intcode.copy())
+boost_two.run(2)
+print(boost_two.get_output())
