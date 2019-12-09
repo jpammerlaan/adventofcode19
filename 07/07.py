d4ee9fff2b8bc492b7d96507bc8b127027f10de7
Job Ammerlaan <job.ammerlaan@transavia.com>
2019-12-09 13:56:21 +0100

    Finished part 2; pretty done with Intcode stuff.
    Changed run_program function to Program class; this makes it a LOT easier to pause and unpause the runnning of the program at will. I think this can be refactored down a lot more, but I'll probably do that once I get around to doing day 9. Pretty happy with this code for now, so I'll leave it be.

--- 07/07.py
+++ 07/07.py
@@ -12,56 +12,93 @@ def get_op_modes(op_str):
     return int(op_str[3:]), modes[::-1]  # reverse the modes list
 
 
-def run_program(intcode, phase, input_val):
-    i = 0
-    num_params = [0, 3, 3, 1, 1, 2, 2, 3, 3]
-    return_val = 0
-    used_phase = False
+class Program:
+    NUM_PARAMS = [0, 3, 3, 1, 1, 2, 2, 3, 3]
 
-    while intcode[i] != 99:
-        op, modes = get_op_modes(str(intcode[i]))
-        # Refactored my get_args() function after seeing the list comprehension from /u/lele3000:
-        args = [intcode[i + j + 1] if modes[j] else intcode[intcode[i + j + 1]] for j in range(num_params[op])]
-        if op == 1:
-            intcode[intcode[i + 3]] = args[0] + args[1]
-        elif op == 2:
-            intcode[intcode[i + 3]] = args[0] * args[1]
-        elif op == 3:
-            intcode[intcode[i + 1]] = input_val if used_phase else phase
-            used_phase = True
-        elif op == 4:
-            return_val = intcode[intcode[i + 1]]
-        elif op == 5:
-            i = args[1] - 3 if args[0] != 0 else i  # offset the increment at the end of this loop by subtracting 3
-        elif op == 6:
-            i = args[1] - 3 if args[0] == 0 else i  # offset the increment at the end of this loop by subtracting 3
-        elif op == 7:
-            intcode[intcode[i + 3]] = 1 if args[0] < args[1] else 0
-        elif op == 8:
-            intcode[intcode[i + 3]] = 1 if args[0] == args[1] else 0
-        else:
-            raise ValueError('Invalid operator code {}'.format(op))
-        i += num_params[op] + 1
-    return return_val
+    def __init__(self, intcode, phase, return_output):
+        self.intcode = intcode
+        self.phase = phase
+        self.used_phase = False
+        self.active = True
+        self.input = None
+        self.output = None
+        self.pause_on_output = return_output
+        self.idx = 0
+
+    @staticmethod
+    def _get_op_modes(op_str):
+        op_str = op_str.zfill(5)  # pad opcode to length 5 so we can always use it the same way
+        modes = list(map(int, op_str[0:3]))  # get modes as ints
+        return int(op_str[3:]), modes[::-1]  # reverse the modes list
+
+    def set_input(self, input):
+        self.input = input
+
+    def get_output(self):
+        return self.output
+
+    def get_active_status(self):
+        return self.active
+
+    def run(self):
+        while self.intcode[self.idx] != 99:
+            op, modes = self._get_op_modes(str(self.intcode[self.idx]))
+            # Refactored my get_args() function after seeing the list comprehension from /u/lele3000:
+            args = [self.intcode[self.idx + j + 1] if modes[j] else self.intcode[self.intcode[self.idx + j + 1]] for j
+                    in range(self.NUM_PARAMS[op])]
+            if op == 1:
+                self.intcode[self.intcode[self.idx + 3]] = args[0] + args[1]
+            elif op == 2:
+                self.intcode[self.intcode[self.idx + 3]] = args[0] * args[1]
+            elif op == 3:
+                self.intcode[self.intcode[self.idx + 1]] = self.input if self.used_phase else self.phase
+                self.used_phase = True
+            elif op == 4:
+                self.output = self.intcode[self.intcode[self.idx + 1]]
+                if self.pause_on_output:
+                    self.idx += self.NUM_PARAMS[op] + 1
+                    return
+            elif op == 5:
+                # offset the increment at the end of this loop by subtracting 3
+                self.idx = args[1] - 3 if args[0] != 0 else self.idx
+            elif op == 6:
+                # offset the increment at the end of this loop by subtracting 3
+                self.idx = args[1] - 3 if args[0] == 0 else self.idx
+            elif op == 7:
+                self.intcode[self.intcode[self.idx + 3]] = 1 if args[0] < args[1] else 0
+            elif op == 8:
+                self.intcode[self.intcode[self.idx + 3]] = 1 if args[0] == args[1] else 0
+            else:
+                raise ValueError('Invalid operator code {}'.format(op))
+            self.idx += self.NUM_PARAMS[op] + 1
+        self.active = False
+        return
 
 
-def find_highest(base_intcode, phase_range):
+def test_program_configuration(programs):
+    active = True
+    output = 0
+    while active:
+        for i, program in enumerate(programs):
+            program.run()
+            active = program.get_active_status()
+            output = program.get_output()
+            programs[(i + 1) % 5].set_input(output)
+    return output
+
+
+def find_highest(base_intcode, phase_range, loop):
     phase_perms = permutations(phase_range)
     highest = 0
     for phases in phase_perms:
-        input_val = 0
-        for phase in phases:
-            input_val = run_program(base_intcode.copy(), phase, input_val)
-        highest = input_val if input_val > highest else highest
+        programs = [Program(intcode=base_intcode.copy(), phase=phase, return_output=loop) for phase in phases]
+        programs[0].set_input(0)  # set initial input value
+        current = test_program_configuration(programs)
+        highest = max(current, highest)
     return highest
 
 
-def find_highest_loop(base_intcode, phase_range):
-    intcodes = [base_intcode.copy() for _ in phase_range]
-    inputs = [0 for _ in phase_range]
-
-
 # part one
-print(find_highest(base_intcode=base_intcode, phase_range=range(0, 5)))
+print(find_highest(base_intcode=base_intcode, phase_range=range(0, 5), loop=False))
 # part two
-print(find_highest_loop(base_intcode=base_intcode, phase_range=range(5, 10)))
+print(find_highest(base_intcode=base_intcode, phase_range=range(5, 10), loop=True))
