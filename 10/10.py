3f71aa48ae0a2d0a32a976111e816cf9aaebb3c7
Job Ammerlaan <job.ammerlaan@transavia.com>
2019-12-12 09:13:18 +0100

    Finished day 10; these coordinates are confusing!
    Had a lot of issues getting the proper angle and translation between all
     the different types of coordinates. I had to draw some of the angles
     out on paper to see if it was really what I wanted to do, but pretty
     happy with what I ended up with as my final solution. Won't be
     refactoring this a whole lot, since I think the comments speak for
     themselves and it's gonna be hard to clean this up more regardless.

--- 10/10.py
+++ 10/10.py
@@ -1,46 +1,38 @@
 from helper_functions.io import read_input_file
-from numpy import exp, abs, angle
+from math import pi, atan2, sqrt, cos, sin
+from collections import defaultdict
 
 DAY = '10'
 input_map = read_input_file(DAY, output_type='list')
-# input_map = """.#..#
-# .....
-# #####
-# ....#
-# ...##""".split('\n')
 
 
-# might need these later
-def to_polar(z):
-    return abs(z), angle(z)
+def to_polar(x, y):
+    return sqrt(x * x + y * y), atan2(x, y)
 
 
-def to_cartesian(r, theta):
-    return r * exp(1j * theta)
+def get_visible_asteroids(asteroids, center):
+    xc, yc = center
+    angles_dict = defaultdict(list)
+    for x, y in asteroids:
+        radius, angle = to_polar(x - xc, yc - y)  # yc -y reverses the y direction to offset the reversed axis
+        angles_dict[angle].append((radius, (x, y)))
+    # sort asteroids in order of radius; ensure angles are in [0, 2pi] rather than [-pi, pi]
+    return {angle + 2 * pi if angle < 0 else angle: sorted(radii) for angle, radii in angles_dict.items()}
 
 
-def get_num_visible(ast, center):
-    angles = [angle(a2 - center) for a2 in ast if a2 != center]
-    return len(set(angles))
+def get_winner(station, visible_asteroids):
+    visible_from_station = visible_asteroids[station]
+    winner_angle = sorted(visible_from_station.keys())[199]  # get angle of winning asteroid
+    winner = visible_from_station[winner_angle][0]  # get the first asteroid in this angle
+    return winner[1]  # return x, y coordinates of winner
 
 
-def vaporize(asteroids, station):
-    i = 0
-    prev_angle = 0
-    polar_asteroids = [to_polar(a - station) for a in asteroids if a != station]
-    sorted_asteroids = sorted(polar_asteroids, key=lambda x: (x[1], x[1]))
-    print(sorted_asteroids)
-    while i <= 200:
-        break
-
-
-
-asteroids = [(x + y*1j) for y, row in enumerate(input_map) for x, c in enumerate(list(row)) if c == '#']
-num_visible = {a: get_num_visible(asteroids.copy(), a) for a in asteroids}
+asteroids = [(x, y) for y, row in enumerate(input_map) for x, c in enumerate(list(row)) if c == '#']
+visible_asteroids = {a: get_visible_asteroids(asteroids.copy(), a) for a in asteroids}
 # part one
+num_visible = {k: len(v) for k, v in visible_asteroids.items()}
 print(max(num_visible.values()))
-
 # part two
 station = max(num_visible, key=num_visible.get)
-vaporize(asteroids.copy(), station)
-
+winner = get_winner(station, visible_asteroids)
+print(100 * winner[0] + winner[1])
